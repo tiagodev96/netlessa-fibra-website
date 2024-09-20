8912e20c7c832f0be84900e0022ac3c3c7907628
Tiago Batista <tiago.dev96@gmail.com>
2024-09-20 19:30:53 -0300

    feat: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    colors: {
+      green: '#01ffa9',
+      'light-green': '#adffe4',
+      blue: '#003875',
+      'light-blue': '#add5ff',
+      gray: '#3d3d3d',
+      black: '#000a14',
+      white: '#fefefe',
+    },
     extend: {},
   },
   plugins: [],
